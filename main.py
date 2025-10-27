7425c482a4478849e797de2d92579b74109893cc
jackson <jackson.dobson2026@gbwl.org>
2025-10-27 16:35:33 +0000

    helo

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import numpy as np
 
 df = pd.read_csv('movies_metadata_v2.csv', encoding='iso-8859-1').dropna(axis=1, how='all')
 print(df.head())
+print(df.shape)
+print(df.info())
+
+budget_df = df[df['budget']>1000000]
+print(budget_df.shape)
+
+budget_lookup = pd.Series[budget_df, ]
